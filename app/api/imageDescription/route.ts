7a31ae9a12220d278d6a20870a723df0dee34e41
Michael Castor <vibecastor@gmail.com>
2025-05-04 12:54:25 -0700

    serveless timeout maxDuratoin increased

--- app/api/imageDescription/route.ts
+++ app/api/imageDescription/route.ts
@@ -1,50 +1,61 @@
-import { generateObject } from 'ai'
-import { openAI } from '@/app/providers/openAI'
-import { outputSchema } from "@/app/schemas/schemas"
-import { prompt } from "@/app/constants/constants"
+import { generateObject } from "ai";
+import { openAI } from "@/app/providers/openAI";
+import { outputSchema } from "@/app/schemas/schemas";
+import { prompt } from "@/app/constants/constants";
+
+export const maxDuration = 50;
 
 // POST /imageDescription
-export async function POST(req: Request) {  
+export async function POST(req: Request) {
   try {
-  const body = await req.json();
+    const body = await req.json();
 
-  const { image, mimeType } = body;
+    const { image, mimeType } = body;
 
-  // Convert the Base64 image string to a data URL
-  const imageUrl = `data:image/png;base64,${image}`;
-  const model = openAI('gpt-4o')
-  
-  // *********************** //
-  // *** openAI endpoint *** //
-  // *********************** //
-  const response = await generateObject({
-    model,
-    output: 'object',
-    schema: outputSchema,
-    messages: [
-      {
-        role: 'user',
-        content: [
-          { type: 'text', text: prompt },
-          { type: 'image', image: imageUrl, mimeType: mimeType,        
-            experimental_providerMetadata: {
-              openai: { imageDetail: 'high' },
-            }, },
-        ],
-      },
-    ],
-    maxTokens: 4096,
-  })
+    // Convert the Base64 image string to a data URL
+    const imageUrl = `data:image/png;base64,${image}`;
+    const model = openAI("gpt-4o");
 
-  return response.toJsonResponse();
-} catch (error) {
-    console.error('[ERROR]: POST/imageDescription:', error);
-
-    return new Response(JSON.stringify({ error: 'Internal Server Error: please try again later' }), {
-      status: 500,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    // *********************** //
+    // *** openAI endpoint *** //
+    // *********************** //
+    const response = await generateObject({
+      model,
+      output: "object",
+      schema: outputSchema,
+      messages: [
+        {
+          role: "user",
+          content: [
+            { type: "text", text: prompt },
+            {
+              type: "image",
+              image: imageUrl,
+              mimeType: mimeType,
+              experimental_providerMetadata: {
+                openai: { imageDetail: "high" },
+              },
+            },
+          ],
+        },
+      ],
+      maxTokens: 4096,
     });
+
+    return response.toJsonResponse();
+  } catch (error) {
+    console.error("[ERROR]: POST/imageDescription:", error);
+
+    return new Response(
+      JSON.stringify({
+        error: "Internal Server Error: please try again later",
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
 }
